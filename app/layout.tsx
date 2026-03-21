ae930dfc6ac44a628651ab673d6b2c56bd0f0d6b
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2026-03-21 21:33:32 +0530

    feat: added project files

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
+
+const SchibstedGrotesk = Schibsted_Grotesk({
+	variable: "--font-schibsted-Grotesk",
+	subsets: ["latin"],
+});
+
+const MartianMono = Martian_Mono({
+	variable: "--font-martian-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "DevEvent",
+	description: "The hub for every dev event",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html
+			lang="en"
+			className={`${SchibstedGrotesk.variable} ${MartianMono.variable} min-h-screen antialiased`}
+		>
+			<body className="min-h-full flex flex-col">
+				<Navbar/>
+				<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+					<LightRays
+						raysOrigin="top-center"
+						raysColor="#5dfeca"
+						raysSpeed={0.5}
+						lightSpread={0.9}
+						rayLength={1.4}
+						followMouse={true}
+						mouseInfluence={0.02}
+						noiseAmount={0}
+						distortion={0}
+						className="custom-rays"
+						pulsating={false}
+						fadeDistance={1}
+						saturation={1}
+					/>
+				</div>
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+}
